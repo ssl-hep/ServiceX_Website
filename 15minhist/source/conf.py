9c5b9c10ae38e20d32577bd98c2d5ccb6f5863aa
Roger Janusiak <rogerja@uw.edu>
2026-02-08 18:34:42 -0600

    First working version of the navbar.

--- 15minhist/source/conf.py
+++ 15minhist/source/conf.py
@@ -25,6 +25,22 @@ extensions = [
 
 templates_path = ['_templates']
 
+html_css_files = [
+    ('https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css',
+     {'crossorigin': 'anonymous'}),
+    ('https://rogerjanusiak.github.io/ServiceX_Website/navbar.css',
+     {'crossorigin': 'anonymous'}),
+    ('https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css',
+     {'crossorigin': 'anonymous'}),
+    
+]
+
+html_js_files = [
+    ('https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js', 
+     {'integrity': 'sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI', 
+      'crossorigin': 'anonymous'}),
+]
+
 exclude_patterns = []
 
 # -- Options for HTML output -------------------------------------------------
